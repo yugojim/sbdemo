fa54903e15b7e2ac8463db72f8a5d13f774edf4c
yugojim <yugojim@gmail.com>
2024-02-23 11:43:04 +0800

    first commit

--- src/test/java/com/example/demo/WebApplicationTests.java
+++ src/test/java/com/example/demo/WebApplicationTests.java
@@ -1,7 +1,4 @@
-package com.example.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
