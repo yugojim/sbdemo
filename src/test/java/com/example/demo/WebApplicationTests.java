ccda1ccf87842456f55360584db0042185b4bd5d
yugojim <yugojim@gmail.com>
2024-02-23 11:02:07 +0800

    first commit

--- src/test/java/com/example/demo/WebApplicationTests.java
+++ src/test/java/com/example/demo/WebApplicationTests.java
@@ -1,38 +1,22 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+@EnableAutoConfiguration
+public class MyApplication {
 
-public class ProductController {
-	@GetMapping("/products/{id}")
-    public Product getProduct(@PathVariable("id") String id) {
-        Product product = new Product();
-        product.setId(id);
-        product.setName("Romantic Story");
-        product.setPrice(200);
-
-        return product;
-}
-
-public class Product {
-    private String id;
-    private String name;
-    private int price;
-
-    public Product() {
-
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
     }
 
-    public Product(String id, String name, int price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
     }
+
 }
