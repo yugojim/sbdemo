0c8e77a81643f150f86d300cf22b256758c05eed
yugojim <yugojim@gmail.com>
2024-02-23 11:34:36 +0800

    first commit

--- src/test/java/com/example/demo/WebApplicationTests.java
+++ src/test/java/com/example/demo/WebApplicationTests.java
@@ -3,11 +3,27 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootTest
 class WebApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+}
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
 
 }
