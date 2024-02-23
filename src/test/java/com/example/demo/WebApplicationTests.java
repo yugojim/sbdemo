d5171e4807ab235069c5d0ff1f01cb4cfaa3f262
yugojim <yugojim@gmail.com>
2024-02-23 10:44:42 +0800

    first commit

--- src/test/java/com/example/demo/WebApplicationTests.java
+++ src/test/java/com/example/demo/WebApplicationTests.java
@@ -2,12 +2,39 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class WebApplicationTests {
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 
-	@Test
-	void contextLoads() {
-	}
+public class ProductController {
+	@GetMapping("/products/{id}")
+    public Product getProduct(@PathVariable("id") String id) {
+        Product product = new Product();
+        product.setId(id);
+        product.setName("Romantic Story");
+        product.setPrice(200);
 
+        return product;
+}
+
+public class Product {
+    private String id;
+    private String name;
+    private int price;
+
+    public Product() {
+
+    }
+
+    public Product(String id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // 略過get與set方法
 }
