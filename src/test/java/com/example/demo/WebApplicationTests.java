75ae4f94ceb2e3eb108eec0fc6f952557b74a7f4
yugojim <yugojim@gmail.com>
2024-02-23 11:28:55 +0800

    first commit

--- src/test/java/com/example/demo/WebApplicationTests.java
+++ src/test/java/com/example/demo/WebApplicationTests.java
@@ -2,15 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootTest
 class WebApplicationTests {
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
+
+	@Test
+	void contextLoads() {
+	}
+
 }
